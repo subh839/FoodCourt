7fa2a64f2dc4063dab6cf18b6764e8b14005d23c
subhasishhalder107@gmail.com <subhasishhalder107@gmail.com>
2025-04-06 23:19:03 +0530

    testing

--- FoodCourt/Models/Class2.cs
+++ FoodCourt/Models/Class2.cs
@@ -2,8 +2,7 @@
 {
     public class Class2
     {
-        public int ProductId { get; set; }
-        public Product Product { get; set; }
+       
         public int IngredientId { get; set; }
         public Ingredient Ingredient { get; set; }
     }
