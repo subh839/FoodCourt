b3a1be4541072eccd302a02f5b866e898363765a
subhasishhalder107@gmail.com <subhasishhalder107@gmail.com>
2025-04-06 23:11:44 +0530

    modified

--- FoodCourt/Controllers/HomeController.cs
+++ FoodCourt/Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace FoodCourt.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
---
+++ FoodCourt/Models/Class2.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodCourt.Models
+{
+    public class Class2
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
